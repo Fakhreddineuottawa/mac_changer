31212ca813687891197f95fc04f282c18075475a
Fakhreddine Bouaziz <71568228+Fakhreddineuottawa@users.noreply.github.com>
2021-05-21 18:40:50 -0400

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,17 +1,22 @@
-#!/usr/bin/env python
 import subprocess
 import optparse
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="the new MAC address")
+    return parser.parse_args()
+
+def change_mac(interface, new_mac) :
+    print("[+] Changing MAC address for " + interface)
+    subprocess.call("ifconfig " + interface + " down", shell=True)
+    subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+    subprocess.call("ifconfig " + interface + " up", shell=True)
+    subprocess.call("ifconfig", shell=True)
 
 
-parser = optparse.OptionParser()
-parser.add_option("-i", "--interface", dest = "interface", help = "Interface to change its MAC address")
-parser.add_option("-m", "--mac", dest = "new_mac", help = "the new MAC address")
-(options, arguments) = parser.parse_args()
+(options, arguments) = get_arguments()
 
 interface = options.interface
 new_mac = options.new_mac
-print("[+] Changing MAC address for " + interface)
-subprocess.call("ifconfig " + interface + " down", shell = True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell = True)
-subprocess.call("ifconfig " + interface + " up", shell = True)
-subprocess.call("ifconfig", shell = True)
+
+change_mac(interface, new_mac)
